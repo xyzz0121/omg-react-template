f12155117cb790fe683c2e84783673805344043b
zhouyang20 <zhouyang20@58ganji.com>
2019-05-04 10:07:24 +0800

    fix:redux entry

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,11 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom';
 
+{{#if isNeedRedux}}
 import store from './store';
 import { Provider } from "react-redux";
+{{/if}}
+
 {{#if isNeedRouter}}
 import { MyRouter } from "./routers";
 {{ else}}
@@ -18,11 +21,9 @@ import FastClick from 'fastclick';
 FastClick.attach(document.body);
 {{/if}}
 
-
 import './assets/style/reset.scss';
 
-
-
+{{#if isNeedRedux}}
 ReactDOM.render(<Provider store={store}>
 	{{#if isNeedRouter}}
     <MyRouter />
@@ -30,3 +31,12 @@ ReactDOM.render(<Provider store={store}>
 		<Home />
 	{{/if}}
 </Provider>, document.getElementById("app"));
+{{ else}}
+ReactDOM.render(
+	{{#if isNeedRouter}}
+    <MyRouter />
+	{{ else}}
+		<Home />
+	{{/if}}
+, document.getElementById("app"));
+{{/if}}
