6d8d37e06911be04ca558c097df9a0dea2ab7753
zhouyang20 <zhouyang20@58ganji.com>
2019-05-04 08:47:44 +0800

    feat:handlebars router

--- src/index.tsx
+++ src/index.tsx
@@ -8,24 +8,28 @@ import * as ReactDOM from 'react-dom';
 
 import store from './store';
 import { Provider } from "react-redux";
-
+{{#if isNeedRouter}}
 import { MyRouter } from "./routers";
+{{ else}}
+import { Home } from "./pages";
+{{/if}}
 
 import FastClick from 'fastclick';
 FastClick.attach(document.body);
 
-
+{{#if isInMobile}} 
 //TODO：上线前干掉
-// import VConsole from 'vconsole/dist/vconsole.min.js' //import vconsole
-// const vConsole = new VConsole(); 
-
+import VConsole from 'vconsole/dist/vconsole.min.js' //import vconsole
+const vConsole = new VConsole(); 
+{{/if}
 import './assets/style/reset.scss';
 
+
+
 ReactDOM.render(<Provider store={store}>
-    {/* {{#if need}}
-        <MyRouter />
-    {{ else}}
-        <p>is not email</p>
-    {{/if}} */}
-    <MyRouter></MyRouter>
+	{{#if isNeedRouter}}
+    <MyRouter />
+	{{ else}}
+		<Home />
+	{{/if}}
 </Provider>, document.getElementById("app"));
