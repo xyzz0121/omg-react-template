354c4261fe8737883359e66716e8b56cf0a9ca18
zhouyang20 <zhouyang20@58ganji.com>
2019-05-04 09:40:57 +0800

    fix:need router

--- src/components/Home/HomeContent/index.tsx
+++ src/components/Home/HomeContent/index.tsx
@@ -7,18 +7,22 @@ import * as React from 'react';
 import "./index.scss";
 import { connect } from 'react-redux';
 import { HomeContentProps } from "./index.d";
+{{#if isNeedRouter}} 
 import { Link } from 'react-router-dom';
+{{/if}}
+
 
 
 
 class HomeContent extends React.Component<HomeContentProps,any> {
-
   render() {
     const { currNum } = this.props;
     return (
       <div className="homeContent">
         <div className="homeContent-number">点了{currNum}次了</div>
-        <Link to='/detail/999'>跳转详情页</Link>
+        {{#if isNeedRouter}} 
+          <Link to={`/detail/${currNum}`}>跳转详情页</Link>
+        {{/if}}
       </div>
     )
   }
